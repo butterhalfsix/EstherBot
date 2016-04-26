8169082769fede2204a15c66ccf06534a6f2cc42
butterhalfsix <mike@butterhalfsix.com>
2016-04-26 15:24:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,8 @@ module.exports = new Script({
         }
     },
       learn: {
-        receive: (bot) => {
-            return bot.say('So you want to learn about Mike? Just say HI to get started.')
+            prompt: (bot) => bot.say('So you want to learn about Mike? Just say HI to get started.')
                 .then(() => 'speak');
-        }
     },
     speak: {
         receive: (bot, message) => {
